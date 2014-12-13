7755c46fffe3c28c454dabf8977765db892660ca
Phil <peva0411@gmail.com>
2014-12-13 17:44:48 -0500

    karma tests for filter and duration service

---
+++ GithubStatusTab/app/js/controllers/MainCtrl.js
@@ -0,0 +1,49 @@
+﻿angular.module('githubStatusApp')
+    .controller('MainCtrl', ['$scope', '$timeout', 'GithubService', 'DurationService', 'UserService', 'alert', function ($scope, $timeout, githubService, durationService, userService, alert) {
+
+    $scope.getMostRecentEvent = function (user) {
+        $scope.isLoading = true;
+
+        var startTime = new Date();
+
+        var updateEvents = function (events) {
+            $scope.event = events[0];
+            $scope.duration = durationService.getDuration(new Date(), events[0].created_at);
+            updateTime();
+        };
+
+        var updateTime = function () {
+            var currentTime = new Date();
+            var diff = Math.abs(currentTime - startTime);
+
+            if (diff > 60000) {
+                startTime = new Date();
+                githubService.getEvents(user)
+                    .then(updateEvents)
+                    .catch(function (reason) { alert(reason); });
+            } else {
+                $scope.duration = durationService.getDuration(new Date(), $scope.event.created_at);
+                $timeout(updateTime, 1000);
+            }
+        };
+
+        githubService.getEvents(user)
+            .then(function (events) {
+                if (events.length > 0) {
+                    $scope.hasEvents = true;
+                    updateEvents(events);
+                } else {
+                    $scope.hasEvents = false;
+                }
+            })
+            .catch(function (reason) {
+                alert('danger', '', reason);
+            }).finally(function () {
+                $scope.isLoading = false;
+            });
+    };
+
+    if (userService.user) {
+        $scope.getMostRecentEvent(userService.user.login);
+    }
+}]);
---
+++ GithubStatusTab/test/unit/DurationService.spec.js
@@ -0,0 +1,39 @@
+﻿'use strict';
+
+describe('DurationService', function() {
+
+    var service;
+    var sinceDate = "2014-01-01";
+
+    beforeEach(module('githubStatusApp'));
+    beforeEach(inject(function (DurationService) {
+        service = DurationService;
+    }));
+
+    var getDuration = function(currentString, sinceString) {
+        var currentDate = new Date(currentString);
+        var sinceDate = new Date(sinceString);
+        return service.getDuration(currentDate, sinceDate);
+    };
+
+    it('should return 1 day duration when dates 1 day apart',
+        function() {
+            var duration = getDuration('1/2/2014', sinceDate);
+            expect(duration.days).toBe(1);
+        });
+
+    it('Should return 1 hour duration when dates 1 hour apart', function() {
+        var duration = getDuration("2014-01-01T01:00:00", sinceDate);
+        expect(duration.hours).toBe(1);
+    });
+
+    it('Should return 1 min duration when dates 1 min apart', function() {
+        var duration = getDuration('2014-01-01T00:01:00', sinceDate);
+        expect(duration.minutes).toBe(1);
+    });
+
+    it('Should return 1 second duration when dates 1 sec apart', function() {
+        var duration = getDuration('2014-01-01T00:00:01', sinceDate);
+        expect(duration.seconds).toBe(1);
+    });
+});
