7b8cd360582e9b3c5e61208e4ffdda586916433f
Phil <peva0411@gmail.com>
2014-12-16 07:28:05 -0500

    Finally got a controller unit test to work

---
+++ GithubStatusTab/app/js/services/GithubService.js
@@ -0,0 +1,33 @@
+﻿angular.module('githubStatusApp').factory('GithubService', ['$q', "$http", function ($q, $http) {
+
+    var apiEndpoint = "https://api.github.com/users";
+
+    var getGithubData = function (user, route) {
+        var defered = $q.defer();
+
+        var requestUrl = [apiEndpoint, user].join('/');
+
+        if (route.length > 0)
+            requestUrl += "/" + route;
+
+        $http.get(requestUrl)
+            .success(function (data) {
+                defered.resolve(data);
+            })
+            .error(function (reason) {
+                defered.reject(reason);
+            });
+
+        return defered.promise;
+
+    };
+
+    return {
+        getEvents: function (user) {
+            return getGithubData(user, 'events');
+        },
+        getUser: function (user) {
+            return getGithubData(user, '');
+        }
+    };
+}]);
--- GithubStatusTab/test/unit/SettingsCtrl.spec.js
+++ GithubStatusTab/test/unit/SettingsCtrl.spec.js
@@ -1,21 +1,72 @@
 ﻿'use strict';
 
-//describe('SettingsCtrl', function() {
+describe('SettingsCtrl', function() {
 
-//    var scope, $controllerConstructor;
+    var scope,
+        $controllerConstructor,
+        $location,
+        userService,
+        githubService,
+        $q,
+        $rootScope,
+        deferred,
+         alert;
 
-//    beforeEach(module('githubStatusApp'));
+    beforeEach(module('githubStatusApp'));
 
-//    beforeEach(inject(function($controller, $rootScope) {
-//        $controllerConstructor = $controller;
-//        scope = $rootScope.$new();
-//    }));
+    beforeEach(module(function($provide) {
+        console.log('start provider set up');
+        userService = {
+            save: function() {}
+        };
 
-//    it('should call getUser when save', function() {
-//        var ctrl = $controllerConstructor("SettingsCtrl",
-//        { $scope: scope, $location: {}, UserService: {}, GithubService: {}, alert: {} });
+        githubService = {
+            getUser: function() {}
+        };
 
-//        expect(true).toBe(true);
+        alert = function() { return "test alert"};
 
-//    });
-//});
+        $provide.value('UserService', userService);
+        $provide.value('GithubService', githubService);
+        $provide.value('alert', alert);
+        console.log('test 1');
+    }));
+
+    beforeEach(inject(function(_$controller_, _$location_, _$rootScope_, _$q_) {
+
+        
+        $controllerConstructor = _$controller_;
+         $rootScope = _$rootScope_;
+        scope = $rootScope.$new();
+
+        $q = _$q_;
+
+        deferred = $q.defer();        
+
+        $location = _$location_;
+
+        spyOn($location, 'path');
+        spyOn(userService, 'save');
+        spyOn(githubService, 'getUser').and.returnValue(deferred.promise);
+
+        console.log(scope);
+        $controllerConstructor('SettingsCtrl', { $scope: scope });
+
+    }));
+
+    describe('When saving a user', function() {
+        it('should call getUser when save', function() {
+           
+            scope.save('test');
+
+            var returnUser = { login: 'test' };
+
+            deferred.resolve(returnUser);
+            $rootScope.$digest();
+
+            expect(userService.save).toHaveBeenCalledWith(returnUser);
+
+        });
+    });
+
+});
